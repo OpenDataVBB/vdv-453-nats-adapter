08d5cdd7d7ecc7b202316a6e10ce7fae161c6339
Jannis R <mail@jannisr.de>
2024-04-16 15:49:52 +0200

    basic VDV-453 client

--- cli.js
+++ cli.js
@@ -21,6 +21,14 @@ const {
 			type: 'boolean',
 			short: 'v',
 		},
+		'leitstelle': {
+			type: 'string',
+			short: 'l',
+		},
+		'endpoint': {
+			type: 'string',
+			short: 'e',
+		},
 	},
 	allowPositionals: true,
 })
@@ -28,9 +36,26 @@ const {
 if (flags.help) {
 	process.stdout.write(`
 Usage:
-    send-vdv-453-data-to-nats [options]
+    send-vdv-453-data-to-nats [options] <service>
+Notes:
+    Valid values for \`service\`:
+    - \`AUS\` subscribes to the VDV-454 AUS service containing network-wide realtime data.
+Options:
+	--leitstelle              -l  VDV-453 Leitstellenkennung, a string identifying this
+	                              client, a bit like an HTTP User-Agent. Must be agreed-
+	                              upon with the provider of the VDV-453 API.
+	                              Default: $VDV_453_LEITSTELLE
+	--endpoint                -e  HTTP(S) URL of the VDV-453 API.
+	                              Default: $VDV_453_ENDPOINT
+	--port                    -p  Port to listen on. VDV-453 requires the client to run an
+	                              HTTP server that the VDV-453 API can call.
+	                              Default: $PORT, otherwise 3000
+Exit Codes:
+	1 – generic and/or unexpected error
+	2 – operation canceled
+	3 – VDV-453 API error
 Examples:
-    send-vdv-453-data-to-nats
+    send-vdv-453-data-to-nats --expires never AUS
 \n`)
 	process.exit(0)
 }
@@ -40,4 +65,81 @@ if (flags.version) {
 	process.exit(0)
 }
 
-// todo
+import {Vdv453ApiError} from 'vdv-453-client'
+import {sendVdv453DataToNats} from './index.js'
+
+const STATUS_CODE_CANCELED = 2
+const STATUS_CODE_VDV_ERROR = 3
+
+const DEBUG = process.env.VDV_453_DEBUG === 'true'
+
+const abortWithError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+const cfg = {}
+const subscriptionOpts = {}
+
+const [service] = args
+if (typeof service !== 'string' || !service) {
+	abortWithError('missing/empty service')
+}
+
+if ('leitstelle' in flags) {
+	cfg.leitstelle = flags.leitstelle
+} else if ('VDV_453_LEITSTELLE' in process.env) {
+	cfg.leitstelle = process.env.VDV_453_LEITSTELLE
+} else {
+	abortWithError('missing/empty --leitstelle/-l/$VDV_453_LEITSTELLE')
+}
+
+if ('endpoint' in flags) {
+	cfg.endpoint = flags.endpoint
+} else if ('VDV_453_ENDPOINT' in process.env) {
+	cfg.endpoint = process.env.VDV_453_ENDPOINT
+} else {
+	abortWithError('missing/empty --endpoint/-e/$VDV_453_ENDPOINT')
+}
+
+if ('port' in flags) {
+	cfg.port = parseInt(flags.port)
+} else if ('PORT' in process.env) {
+	cfg.port = parseInt(process.env.PORT)
+} else {
+	cfg.port = 3000
+}
+
+cfg.subscriptions = [
+	{
+		...subscriptionOpts,
+		service,
+	},
+]
+
+// todo [breaking]: create pino logger here, pass it in?
+let stop = async () => {} // no-op
+try {
+	const _ = await sendVdv453DataToNats(cfg)
+	stop = _.stop
+} catch (err) {
+	// todo: special handling for Vdv453HttpError too?
+	if (!DEBUG && err instanceof Vdv453ApiError) {
+		process.stderr.write(err.message + '\n')
+		process.exit(STATUS_CODE_VDV_ERROR)
+	}
+	abortWithError(err)
+}
+
+let shouldHardExit = false
+process.on('SIGINT', () => {
+	// try to soft-exit on first SIGINT, then hard-exit with status code 1
+	if (shouldHardExit) {
+		process.exit(STATUS_CODE_CANCELED)
+	}
+	shouldHardExit = true
+
+	process.stderr.write('unsubscribing\n')
+	// todo: set timeout?
+	stop().catch(abortWithError)
+})
--- index.js
+++ index.js
@@ -1,5 +1,108 @@
-// todo
+import {ok} from 'node:assert'
+import {createClient} from 'vdv-453-client'
+
+const sendVdv453DataToNats = async (cfg, opt = {}) => {
+	const {
+		leitstelle,
+		endpoint,
+		port,
+		subscriptions,
+	} = cfg
+	ok(leitstelle, 'missing/empty cfg.leitstelle')
+	ok(endpoint, 'missing/empty cfg.endpoint')
+	ok(Number.isInteger(port), 'cfg.port must be an integer')
+	ok(Array.isArray(subscriptions), 'cfg.subscriptions must be an array')
+	ok(subscriptions.length > 0, 'cfg.subscriptions must not be empty')
+	for (let i = 0; i < subscriptions.length; i++) {
+		const {
+			service,
+		} = subscriptions[i]
+		ok(service, `invalid/empty cfg.subscriptions[${i}].service`)
+	}
+
+	const {
+		vdv453ClientOpts,
+	} = {
+		vdv453ClientOpts: {},
+		...opt,
+	}
+
+	const client = createClient({
+		...vdv453ClientOpts,
+		leitstelle,
+		endpoint,
+	})
+	const {
+		logger,
+	} = client
+
+	await new Promise((resolve, reject) => {
+		client.httpServer.listen(port, (err) => {
+			if (err) reject(err)
+			else resolve()
+		})
+	})
+	logger.info(`listening on port ${port}`)
+
+	const subscribeToAUS = () => {
+		let aboId = null
+		const startPromise = client.ausSubscribe({
+			expiresAt: Date.now() + 10 * 60 * 1000, // 10m from now, vdv-453-client uses milliseconds
+		})
+		.then(({aboId: _aboId}) => {
+			aboId = _aboId
+		})
+
+		// todo: process other AUSNachricht children
+		client.data.on('aus:IstFahrt', (istFahrt) => {
+			// todo
+		})
+
+		const unsubscribeFromAUS = async () => {
+			if (aboId === null) return;
+			await client.ausUnsubscribe(aboId)
+		}
+
+		return {
+			startPromise,
+			stopTask: unsubscribeFromAUS,
+		}
+	}
+
+	const startPromises = []
+	const stopTasks = []
+
+	const stop = async () => {
+		await Promise.all(stopTasks.map(task => task()))
+		client.httpServer.close()
+	}
+
+	for (const subscription of subscriptions) {
+		const {
+			service,
+		} = subscription
+		if (subscription.service === 'AUS') {
+			const {
+				startPromise,
+				stopTask,
+			} = subscribeToAUS()
+			startPromises.push(startPromise)
+			stopTasks.push(stopTask)
+		} else {
+			throw new Error(`invalid/unsupported service "${service}"`)
+		}
+	}
+
+	// todo: stop successfully started subscriptions if one of these fails
+	await Promise.all(startPromises)
+
+	return {
+		logger,
+		client,
+		stop,
+	}
+}
 
 export {
-	// todo
+	sendVdv453DataToNats,
 }
